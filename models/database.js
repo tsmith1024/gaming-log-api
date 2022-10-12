130d03354670121f3ef68e33802955a29159362b
Taylor <tsmith1024@gmail.com>
2022-10-12 11:35:13 -0500

    update project setup

--- models/database.js
+++ models/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
-const db = new Sequelize("dockerExpressDemo", "postgres", "postgres", {
+const dbName = process.env.DB_NAME;
+const db = new Sequelize(dbName, "postgres", "postgres", {
   host: "db", // host is 'db' because of name of linked service in docker-compose
   dialect: "postgres",
 });
