6111caa66a176a3a0503d70210cb81bb39299ac9
Taylor <tsmith1024@gmail.com>
2022-12-06 16:12:38 -0600

    update to use env vars

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,37 +1,45 @@
-const { User } = require("../models/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { User } = require("../models/user")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { JWT_ISSUER, JWT_SECRET } = require("../utilities/constants")
 
 // create signin handler
 const signin = async (req, res, next) => {
   // get email and password from req.body
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  let user;
+  let user
   // find user with that email
   try {
     user = await User.findOne({
       where: {
         email: email,
       },
-    });
+    })
   } catch (error) {
     // if unable to find, return 404
     res.status(404).json({
       message: "unable to find user with matching email and password",
       error,
-    });
-    return;
+    })
+    return
+  }
+
+  if (user === null) {
+    res.status(404).json({
+      message: "unable to find user with matching email and password",
+    })
+    return
   }
 
   // compare password submitted and passwordHash
-  let match = bcrypt.compareSync(password, user.passwordHash);
+  let match = bcrypt.compareSync(password, user.passwordHash)
   if (!match) {
     // if they don't match, return 401
     res.status(401).json({
       message: "unable to find user with matching email and password",
-    });
-    return;
+    })
+    return
   }
 
   // if match is found
@@ -42,14 +50,14 @@ const signin = async (req, res, next) => {
       name: user.firstName,
       admin: false,
     },
-    "secret", // the secret key to the JWT
+    JWT_SECRET, // the secret key to the JWT
     {
       expiresIn: "1h",
-      issuer: "org.edfarm.gaming-api",
+      issuer: JWT_ISSUER,
     }
-  );
+  )
   // return that token
-  return res.json({ token });
-};
+  return res.json({ token })
+}
 
-module.exports = { signin };
+module.exports = { signin }
--- models/database.js
+++ models/database.js
@@ -1,8 +1,8 @@
-const { Sequelize } = require("sequelize");
-const dbName = process.env.DB_NAME;
-const db = new Sequelize(dbName, "postgres", "postgres", {
+const { Sequelize } = require("sequelize")
+const { DB_NAME } = require("../utilities/constants")
+const db = new Sequelize(DB_NAME, "postgres", "postgres", {
   host: "db", // host is 'db' because of name of linked service in docker-compose
   dialect: "postgres",
-});
+})
 
-module.exports = { db };
+module.exports = { db }
