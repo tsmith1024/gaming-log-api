1cad0655c1002602eed11315dd3d79fd38a28155
Taylor <tsmith1024@gmail.com>
2022-12-06 16:12:19 -0600

    add token verification middleware to games list api

--- routes/games.js
+++ routes/games.js
@@ -1,23 +1,25 @@
-const express = require("express");
+const express = require("express")
+const logger = require("../middleware/logger")
 const {
   getGames,
   createGame,
   getGame,
   updateGame,
   deleteGame,
-} = require("../controllers/games");
+} = require("../controllers/games")
+const { verifyToken } = require("../middleware/auth")
 
-const router = express.Router();
+const router = express.Router()
 
 // router.get("/", getGames);
 // router.post("/", createGame);
 
 // "/" handlers shorthand
 // localhost:3000/games
-router.route("/").get(getGames).post(createGame);
+router.route("/").get(verifyToken, getGames).post(createGame)
 
 // "/:id" handlers
 // localhost:3000/games/:id
-router.route("/:id").get(getGame).put(updateGame).delete(deleteGame);
+router.route("/:id").get(getGame).put(updateGame).delete(deleteGame)
 
-module.exports = router;
+module.exports = router
