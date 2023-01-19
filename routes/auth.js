ba99b649fd7d103e14a0a76ad47688852cbcacec
Taylor <tsmith1024@gmail.com>
2023-01-19 16:12:10 -0600

    add resetPassword route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,16 @@
 const express = require("express")
 
-const { signin, requestRecovery } = require("../controllers/auth")
+const {
+  signin,
+  requestRecovery,
+  recoverAccount,
+} = require("../controllers/auth")
 
 const router = express.Router()
 
 // signin
 router.route("/signin").post(signin)
 router.route("/recover").post(requestRecovery)
+router.route("/resetPassword").post(recoverAccount)
 
 module.exports = router
